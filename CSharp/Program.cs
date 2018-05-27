1b81d1fa14a4257c12a15a274f4ef83f9cd59c44
buybackoff <vbaybekov@gmail.com>
2018-05-27 15:34:59 +0300

    Benchmarks
    **main**
    *F#*
    
     Case                |    MOPS |  Elapsed |   GC0 |   GC1 |   GC2 |  Memory
    ---------            |--------:|---------:|------:|------:|------:|--------:
    F# Long              |   45.46 |    22 ms |   0.0 |   0.0 |   0.0 | 0.008 MB
    F# Ulong             |   37.04 |    27 ms |   3.0 |   0.0 |   0.0 | 4.891 MB
    
    **Main**
    *C#*
    
     Case                |    MOPS |  Elapsed |   GC0 |   GC1 |   GC2 |  Memory
    ---------            |--------:|---------:|------:|------:|------:|--------:
    C# Ulong             | 5000.00 |     2 ms |   0.0 |   0.0 |   0.0 | 0.000 MB
    C# Long              | 5000.00 |     2 ms |   0.0 |   0.0 |   0.0 | 0.000 MB

---
+++ CSharp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Spreads.Utils;
+
+namespace CSharpTest
+{
+
+    class Worker
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public T Work<T>(T x)
+        {
+            if (typeof(T) == typeof(ulong))
+            {
+                var u = (ulong)(object)x;
+                u = u + 1;
+                return (T)(object)u;
+            }
+            else
+            {
+                return x;
+            }
+        }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            var worker = new Worker();
+            for (int round = 0; round < 20; round++)
+            {
+                var sum1 = 0UL;
+                using (Benchmark.Run("C# Ulong", 10000000, true))
+                {
+                    
+                    for (var i = 0UL; i < 10000000UL; i++)
+                    {
+                        sum1 += worker.Work(i);
+                    }
+                }
+                var sum2 = 0L;
+                using (Benchmark.Run("C# Long", 10000000, true))
+                {
+                    for (var i = 0L; i < 10000000L; i++)
+                    {
+                        sum2 += worker.Work(i);
+                    }
+                }
+
+                if (sum1 + (ulong)sum2 <= 10000)
+                {
+                    throw new Exception();
+                }
+            }
+
+            Benchmark.Dump("C#");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.fs
@@ -0,0 +1,43 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+open System.Threading
+open Spreads.Utils
+open System.Runtime.CompilerServices
+
+type Worker() =
+  [<MethodImplAttribute(MethodImplOptions.AggressiveInlining)>]
+  member this.Work<'T>(x: 'T) : 'T =
+    if typeof<'T> = typeof<uint64> then
+      let d : uint64 = unbox(box(x)) // unbox(box(x))
+      unbox(box(d))
+    else 
+      x
+
+[<EntryPoint>]
+let main argv =
+  let worker = Worker()
+  for round in 0..20 do
+    let bench1 = 
+      use b1 = Benchmark.Run("F# Ulong", 1000000, true)
+      let mutable output : uint64 = Unchecked.defaultof<_>
+      for i in 0UL..1000000UL do
+        output <- output + (worker.Work(i));
+      output
+
+    let bench2 = 
+      use b2 = Benchmark.Run("F# Long", 1000000, true)
+      let mutable output : int64 = Unchecked.defaultof<_>
+      for i in 0L..1000000L do
+        output <- output + (worker.Work(i));
+      output
+
+    if int64(bench1) + bench2 < 0L then failwith ""
+
+  Benchmark.Dump("F#")
+
+  CSharpTest.Program.Main()
+
+  Console.ReadLine() |> ignore
+  0 // return an integer exit code
